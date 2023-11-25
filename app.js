69715b29cf4626883763a48cbad5b4b2f479b27f
rbsingh001 <129167801+rbsingh001@users.noreply.github.com>
2023-11-25 17:07:15 +0530

    diffrent routes in express
    add-product ,/,product

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use((req, res, next)=>{
-    console.log('middleware');
-    next()
+app.use('/add-product',(req, res, next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>')
 })
 
-app.use((req, res, next)=>{
-    console.log('another middleware');
-    res.send('<h1>Hello from express </h1>')
+app.use('/product',(req, res, next)=>{
+    console.log(req.body)
+    res.redirect('/')
+})
+app.use('/', (req, res, next)=>{
+    res.send('<h1>Hello from Express!</h1>')
 })
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
